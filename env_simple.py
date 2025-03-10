67aabae6630be8be58c55de20282b41e6641ebbe
raouzair10 <muzair.bese21seecs@seecs.edu.pk>
2025-03-10 11:27:11 +0500

    Initial commit

---
+++ env_simple.py
@@ -0,0 +1,205 @@
+import numpy as np
+from scipy.special import lambertw
+import math
+dtype = np.float32
+
+class Env_cellular():
+    def __init__(self, MAX_EP_STEPS, s_dim, location_PDs, location_SDs, Emax, K, T, eta, Pn, Pmax, w_csk, fading_n, fading_0):
+        self.emax = Emax        # Maximum battery capacity for SD
+        self.K = K              # Number of Primary Devices (PDs)
+        self.T = T              # Time duration for each time slot
+        self.eta = eta          # Energy harvesting efficiency (between 0 and 1)
+        self.Pn = Pn            # transmit power of PDs
+        self.Pmax = Pmax        # Maximum transmit power of the SD
+        self.w_csk = w_csk      # circuit and signal processing power for the SD
+        self.s_dim = s_dim      # dimsion of states
+        self.MAX_EP_STEPS = MAX_EP_STEPS  # Max steps per episode
+        
+        BW = 10 ** 6    # Bandwidth of 1 MHz
+        sigma2_dbm = -170 + 10 * np.log10(BW)           # Thermal noise power in dBm, considering the bandwidth
+        self.noise = 10 ** ((sigma2_dbm - 30) / 10)     # Noise power in linear scale (Watts)
+
+        distance_SD_PD = np.sqrt(np.sum((location_PDs - location_SDs) ** 2, axis=1)) # Distance from each PD user to the SD user
+        distance_BS_PD = np.sqrt(np.sum((location_PDs) ** 2, axis=1))               # Distance from each PD user to the base station
+        distance = np.matrix.transpose(np.vstack((distance_SD_PD, distance_BS_PD)))    # Combined distances in a matrix
+        distance = np.maximum(distance, np.ones((self.K, 2)))                          # Ensures minimum distance of 1 meter to avoid singularities
+
+        PL_alpha = 3                                    # Path loss exponent, typically between 2 and 4
+        PL = 1 / (distance ** PL_alpha) / (10 ** 3.17)  # Path loss for each link, considering the distance and path loss exponent
+        self.hn = np.multiply(PL, fading_n)             # Effective channel gains, incorporating path loss and fading
+
+        print('gains from PD to SD and BS')
+        print(self.hn)
+
+        distance_SD_BS = np.maximum(np.sqrt(np.sum(location_SDs ** 2, axis=1)), 1)    # Distance from SD to the base station
+        PL0 = 1 / (distance_SD_BS ** PL_alpha) / (10 ** 3.17)
+        self.h0 = fading_0 * PL0    # Channel gain between SD and the base station
+
+        print('gains from SD to BS')
+        print(self.h0)
+
+        self.channel_sequence = np.zeros((self.MAX_EP_STEPS, 2))    # Stores the channel gains for each time step
+        for i in range(self.MAX_EP_STEPS):
+            id_index = i % self.K
+            self.channel_sequence[i, :] = self.hn[id_index, :]
+        print(self.channel_sequence)
+
+    # Calculates the next state, reward, energy harvested, and whether the episode is done
+    # Parameters:
+        # action: The agent's action, a value between 0 and 1 indicating the proportion of time spent on energy harvesting.
+        # state: Current state vector.
+        # j: Current time step index
+    def step(self, action, state, j):
+        hn = state[0, 0] / self.noise   # Normalized PD-SD channel gain
+        hn0 = state[0, 1]               # PD-BS channel gain
+        h0 = self.h0 / self.noise       # Normalized SD-BS channel gain
+        En = state[0, -1]               # Current battery level
+
+        # Net change in battery energy (harvested - consumed)
+        En_bar = action * min(self.emax - En, self.T * self.eta * self.Pn * hn0) - (1 - action) * min(En, self.T * self.Pmax + self.w_csk)
+        
+        # Intermediate Variables for Reward Calculation
+        mu1 = self.eta * self.Pn * hn0 * h0 / (1 + self.Pn * hn) # Represents the impact of energy harvesting on potential transmission power
+        mu2 = En_bar * h0 / self.T / (1 + self.Pn * hn)          # Adjusts for energy change per unit time
+        mu3 = self.w_csk * h0 / (1 + self.Pn * hn)               # Impact of circuit power consumption
+        
+        # Calculate Optimal Time Allocation
+        wx0 = np.real(lambertw(math.exp(-1) * (mu1 - 1), k=0))
+        alphaxx = (mu1 - mu2) / (math.exp(wx0 + 1) - 1 + mu1 + mu3)
+        alpha01 = 1 - (En + En_bar) / (self.T * self.eta * self.Pn * hn0)
+        alpha02 = (self.T * self.eta * self.Pn * hn0 - En_bar) \
+                  / (self.T * self.eta * self.Pn * hn0 + self.T * self.Pmax + self.T * self.w_csk)
+        alphax2 = max(alpha01, alpha02)
+        alphan = min(1, max(alphaxx, alphax2))  # Optimal fraction of time allocated for data transmission
+        
+        # Compute Reward
+        reward = [0]
+        if En_bar == self.T * self.eta * self.Pn * hn0:
+            P0n=0
+            reward = [0]  # If no energy is available or allocated, reward is zero
+        elif alphan == 0: #<= 0.00000001:
+            P0n=0
+            reward = [0]
+        else:
+            # Compute transmission power (P0n) and reward based on the Shannon capacity formula
+            P0n = (1 - alphan) * self.eta * self.Pn * hn0 / alphan - En_bar / alphan / self.T - self.w_csk
+            if P0n < 0:
+                P0n = 0
+            reward = alphan * np.log(1 + P0n * h0 / (1 + self.Pn * hn))
+
+        # Handle NaN Rewards
+        if math.isnan(reward[0]):
+            reward = [0]
+
+        # Initialize data_rate and energy_consumed with default values
+        data_rate = 0
+        energy_consumed = 0
+
+        # Energy Efficiency Calculation
+        data_rate = alphan * np.log(1 + P0n * h0 / (1 + self.Pn * hn))  # Shannon's capacity formula
+        energy_consumed = (P0n + self.w_csk) * self.T  # Energy consumed
+        energy_efficiency = data_rate / energy_consumed  # Data rate per unit energy
+
+        batter_new = min(self.emax, En + En_bar) # Updated battery level after accounting for energy harvested and consumed
+        
+        # Next state, including updated channel gains and battery level
+        state_next = self.channel_sequence[(j+1) % self.K, :].tolist()
+        state_next.append(float(batter_new))
+        state_next = np.reshape(state_next, (1, self.s_dim))
+
+        # Represents the actual energy harvested during the time allocated for energy harvesting
+        EHD = (1 - alphan) * self.eta * self.T * self.Pn * hn0
+
+        done=False  # Episode continues
+        return reward, state_next, EHD, done, energy_efficiency,
+
+    # Simulates a greedy policy where the GF user transmits as much as possible given the battery constraints
+    def step_greedy(self,   state, j):
+        hn = state[0,0]/self.noise
+        hn0 = state[0,1]
+        h0 = self.h0/self.noise
+        En = state[0,-1]
+        alphan = min(1, En/self.T/self.Pmax) # Allocates maximum possible time for data transmission without exceeding battery capacity
+        
+        # Compute Reward
+        if alphan==0:
+            reward = 0
+        else:
+            reward = alphan*np.log(1 + self.Pmax*h0/(1 + self.Pn*hn))
+
+        if math.isnan(reward):
+            print(f"alpha is {alphan}  ")
+            print(f"{En}")
+        
+        # Energy Efficiency Calculation
+        if alphan > 0 and self.Pmax > 0:
+            data_rate = alphan * np.log(1 + self.Pmax * h0 / (1 + self.Pn * hn))  # Shannon's capacity formula
+            energy_consumed = (self.Pmax * alphan + self.w_csk) * self.T  # Energy consumed
+            if energy_consumed > 0:
+                energy_efficiency = data_rate / energy_consumed  # Data rate per unit energy
+            else:
+                energy_efficiency = 0
+        else:
+            energy_efficiency = 0
+        
+        # Update Battery Level
+        batter_new = min(self.emax, En - alphan*self.T*self.Pmax + (1-alphan) * self.T*self.eta*self.Pn*hn0)
+        
+        # Calculate Energy Harvested
+        EHG = (1-alphan)*self.T*self.eta*self.Pn*hn0
+        
+        # Prepare Next State and Return Values
+        state_next = self.channel_sequence[(j+1) % self.K, :].tolist()
+        state_next.append(batter_new)
+        state_next = np.reshape(state_next, (1, self.s_dim))
+        done = False
+        return reward, state_next, EHG, done, energy_efficiency,
+
+    # Simulates a random policy where alphan is chosen randomly within feasible limits
+    def step_random(self,   state, j):
+        hn = state[0,0]/self.noise
+        hn0 = state[0,1]
+        h0 = self.h0/self.noise
+        En = state[0,-1]
+        alphan = np.random.uniform(0, min(1, En/self.T/self.Pmax)) # Randomly Select Transmission Time (alphan)
+        
+        # Compute Reward
+        if alphan==0:
+            reward = 0
+        else:
+            reward = alphan*np.log(1 + self.Pmax*h0/(1 +self.Pn*hn))
+
+        if math.isnan(reward):
+            print(f"alpha is {alphan}  ")
+            print(f"{En}")
+        
+        # Energy Efficiency Calculation
+        if alphan > 0 and self.Pmax > 0:
+            data_rate = alphan * np.log(1 + self.Pmax * h0 / (1 + self.Pn * hn))  # Shannon's capacity formula
+            energy_consumed = (self.Pmax * alphan + self.w_csk) * self.T  # Energy consumed
+            if energy_consumed > 0:
+                energy_efficiency = data_rate / energy_consumed  # Data rate per unit energy
+            else:
+                energy_efficiency = 0
+        else:
+            energy_efficiency = 0
+        
+        # Update Battery Level
+        batter_new = min(self.emax, En -alphan*self.T*self.Pmax +(1-alphan)*self.T*self.eta*self.Pn*hn0)
+        
+        # Calculate Energy Harvested
+        EHR = (1-alphan)*self.T*self.eta*self.Pn*hn0
+        
+        # Prepare Next State and Return Values
+        state_next = self.channel_sequence[(j+1) % self.K, :].tolist()
+        #state_next.append(self.h0)
+        state_next.append(batter_new)
+        state_next = np.reshape(state_next, (1, self.s_dim))
+        done=False
+        return reward, state_next, EHR, done, energy_efficiency,
+
+    # Resets the environment to its initial state with the GF user's battery fully charged
+    def reset(self):
+        batter_ini = self.emax
+        return batter_ini
+print("Successfully Compiled")
---
+++ main.py
@@ -0,0 +1,297 @@
+import numpy as np
+import torch
+import time
+import random
+from env_simple import Env_cellular as env_simple
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from ddpg import Agent as DDPG_AGENT
+import warnings;
+warnings.filterwarnings('ignore')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.patches import ConnectionPatch, Rectangle
+##################### System Parameters ####################
+Emax = 0.3              # Maximum battery capacity for SD
+K = 2                   # Number of Primary Devices (PDs)
+B = 2                   # Number of base station antennas
+T = 1                   # Time duration for each time slot
+eta = 0.7               # Energy harvesting efficiency (between 0 and 1)
+Pn = 1                  # transmit power of PDs
+Pmax = 0.2              # Maximum transmit power of the SD
+w_csk = 0.000003        # circuit and signal processing power for the SD
+# w_d = 1.5 * (10 ** -5)  # Processing power for diversity combining
+# w_egc = 1 * (10 ** -6)  # Processing power for EGC
+# w_mrc = 2 * (10 ** -6)  # Processing power for MRC
+
+##################### Hyper Parameters #####################
+MAX_EPISODES = 10
+MAX_EP_STEPS = 5
+LR_A = 0.0002    # learning rate for actor
+LR_C = 0.0004    # learning rate for critic
+GAMMA = 0.9     # reward discount
+TAU = 0.01      # soft replacement
+MEMORY_CAPACITY = 10000
+BATCH_SIZE = 32
+s_dim = 3       # dimsion of states
+a_dim = 1       # dimension of action
+a_bound = 1     # bound of action
+state_am = 1000
+verbose = True
+
+##################### Set random seed number #####################
+seed = 0
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+##################### User Input for Number of SDs ####################
+num_SDs = int(input("Enter the number of Secondary Devices (at least 1): "))
+
+###################### Network Nodes Deployment  #######################
+location_PDs = np.array([[0, 1],
+                            [0,1000]])   # locations of PDs
+
+# Randomly generate SD locations within a specific range (0 to 10 for both x and y)
+location_SDs = np.random.uniform(low=0, high=10, size=(num_SDs, 2)).astype(int)
+
+############################### Fadings ################################
+
+#### fixed fading value for the SD, set to 1 for simplicity ####
+fading_0 = 1
+
+# # hnx[0, 0] = 0.78954 is the fading value for the first PD on the first path
+# hnx = np.array([[0.78954, 0.62134, 0.62134, 0.75324, 0.98921, 0.85324, 0.95324, 0.65324, 0.79324, 0.92324],
+#                 [0.75324, 0.77954, 0.85324, 0.75324, 0.95324, 0.88324, 0.81324, 0.80324, 0.65324, 0.95324]])
+
+# Generate hnx randomly for each PD and each path
+hnx = np.random.uniform(low=0.6, high=0.99, size=(K, B))
+hnx = np.around(hnx, decimals=5)
+
+# Gains between PD and Base Station
+hnPD = np.array([0.79643, 0.87451])
+
+# placeholder array initialized with zeros to store aggregated fading values for each PD
+fading_n_ = np.zeros(K)
+
+##################### Gains Passing Approach ##########################
+for i in range(K):
+    fading_n_[i] = random.choice(hnx[i])
+fading_n = np.vstack((fading_n_, hnPD))
+fading_n = np.matrix.transpose(fading_n)
+myenv = env_simple(MAX_EP_STEPS, s_dim, location_PDs, location_SDs, Emax, K, T, eta, Pn, Pmax, w_csk, fading_n, fading_0)
+
+#---------------------------------Initializing DDPG POLICY--------------------------------------------------------------
+ddpg_agent=DDPG_AGENT(LR_A,LR_C,s_dim,TAU,a_bound,GAMMA,n_actions=1,max_size=MEMORY_CAPACITY,batch_size=BATCH_SIZE)
+#-----------------------------------------------------------------------------------------------------------------------
+var = 1  # control exploration
+total_time = 1
+t_0 = time.time()
+
+ep_rewardall_ddpg = []
+ep_rewardall_greedy = []
+ep_rewardall_random = []
+
+eh_rewardall_ddpg = []
+eh_rewardall_greedy = []
+eh_rewardall_random = []
+
+ee_rewardall_ddpg = []
+ee_rewardall_greedy = []
+ee_rewardall_random = []
+
+for i in range(MAX_EPISODES):
+    batter_ini = myenv.reset()
+    s = myenv.channel_sequence[i % myenv.K, :].tolist()  # the current GB user, 2 element [GB-GF, GB-BS]
+    s.append(batter_ini)
+    s = np.reshape(s, (1, s_dim))
+    s = s * state_am  # amplify the state
+    s_ddpg = s
+    s_greedy = s
+    s_random = s
+
+    ep_ee_ddpg = 0
+    ep_ee_random = 0
+    ep_ee_greedy = 0
+
+    ep_reward_ddpg = 0
+    ep_reward_random = 0
+    ep_reward_greedy = 0
+
+    eh_reward_ddpg = 0
+    eh_reward_random = 0
+    eh_reward_greedy = 0
+
+    s_traj_ddpg = []
+    s_traj_random = []
+    s_traj_greedy = []
+
+    for j in range(MAX_EP_STEPS):
+        ######################## DDPG ########################
+        a_ddpg = ddpg_agent.choose_action(s_ddpg)
+        a_ddpg = np.clip(np.random.normal(a_ddpg, var), 0, 1)  # add randomness to action selection for exploration
+        r_ddpg, s_ddpg_, EHD, done, ee_ddpg = myenv.step(a_ddpg, s_ddpg / state_am, j)
+        s_ddpg_ = s_ddpg_ * state_am
+        s_traj_ddpg.append(s_ddpg_)
+        ddpg_agent.remember(s_ddpg[0], a_ddpg, r_ddpg, s_ddpg_[0], 0)
+        ep_reward_ddpg += int(*r_ddpg)
+        eh_reward_ddpg += EHD
+        ep_ee_ddpg += ee_ddpg
+        ######################## Greedy ########################
+        r_greedy, s_next_greedy, EHG, done, ee_greedy = myenv.step_greedy(s_greedy / state_am, j)
+        s_traj_greedy.append(s_next_greedy)
+        s_greedy = s_next_greedy * state_am
+        ep_reward_greedy += (r_greedy)
+        eh_reward_greedy += EHG
+        ep_ee_greedy += ee_greedy
+        ######################## Random ########################
+        r_random, s_next_random, EHR, done, ee_random = myenv.step_random(s_random / state_am, j)
+        s_traj_random.append(s_next_random)
+        s_random = s_next_random * state_am
+        ep_reward_random += r_random
+        eh_reward_random += EHR
+        ep_ee_random += ee_random
+        ########################### Network Trainings ######################
+        if var > 0.1:
+            var *= .9998  # decay the action randomness
+        # ----------------DDPG ALGORITHM TRAINING-----------------------
+        ddpg_agent.learn()
+        ########################### Update States ######################
+        s_ddpg = s_ddpg_
+        total_time += 1
+
+        if j == MAX_EP_STEPS-1:
+        #     print('Episode:', i, ' DDPG: %i' % int(ep_reward_ddpg), 'TD3: %i' % int(ep_reward_td3), 'PPO: %i' % int(ep_reward_ppo),
+        #            'Greedy: %i' % int(ep_reward_greedy), 'Random: %i' % int(ep_reward_random))
+            print('Avg EE for Episode:', i, ' DDPG: %i' % int(ep_ee_ddpg/MAX_EP_STEPS), 'Greedy: %i' % int(ep_ee_greedy/MAX_EP_STEPS),  'Random: %i' % int(ep_ee_random/MAX_EP_STEPS))
+            break
+    ep_reward_ddpg = np.reshape(ep_reward_ddpg/MAX_EP_STEPS, (1,))
+    ep_rewardall_ddpg.append(ep_reward_ddpg)
+    eh_reward_ddpg = np.reshape(eh_reward_ddpg / MAX_EP_STEPS, (1,))
+    eh_rewardall_ddpg.append(eh_reward_ddpg)
+    ep_ee_ddpg = np.reshape(ep_ee_ddpg/MAX_EP_STEPS, (1,))
+    ee_rewardall_ddpg.append(ep_ee_ddpg)
+
+    ep_reward_greedy = np.reshape(ep_reward_greedy/MAX_EP_STEPS, (1,))
+    ep_rewardall_greedy.append(ep_reward_greedy)
+    eh_reward_greedy = np.reshape(eh_reward_greedy / MAX_EP_STEPS, (1,))
+    eh_rewardall_greedy.append(eh_reward_greedy)
+    ep_ee_greedy = np.reshape(ep_ee_greedy/MAX_EP_STEPS, (1,))
+    ee_rewardall_greedy.append(ep_ee_greedy)
+
+    ep_reward_random = np.reshape(ep_reward_random/MAX_EP_STEPS, (1,))
+    ep_rewardall_random.append(ep_reward_random)
+    eh_reward_random = np.reshape(eh_reward_random / MAX_EP_STEPS, (1,))
+    eh_rewardall_random.append(eh_reward_random)
+    ep_ee_random = np.reshape(ep_ee_random/MAX_EP_STEPS, (1,))
+    ee_rewardall_random.append(ep_ee_random)
+
+
+# ######### CALCULATING AVERAGE REWARDS AND EH ##########
+avg_rewards = [sum(ep_rewardall_ddpg)/len(ep_rewardall_ddpg),
+                sum(ep_rewardall_greedy)/len(ep_rewardall_greedy),
+                sum(ep_rewardall_random)/len(ep_rewardall_random)]
+
+avg_harvested_energy = [sum(eh_rewardall_ddpg)/len(eh_rewardall_ddpg),
+                        sum(eh_rewardall_greedy)/len(eh_rewardall_greedy),
+                        sum(eh_rewardall_random)/len(eh_rewardall_random)]
+
+print("=================================================================================")
+########### PLOTTING FIGURES ###############
+fig, ax = plt.subplots()
+ax.plot(ep_rewardall_ddpg, "^-", label='DDPG', linewidth=0.75 , color= 'darkblue')
+ax.plot(ep_rewardall_greedy, "o-", label='Greedy', linewidth=0.75)
+ax.plot(ep_rewardall_random, "x-", label='Random', color='black', linewidth=0.75)
+ax.set_xlabel("Episodes")
+ax.set_ylabel("Epsiodic Rewards")
+ax.legend()
+ax.margins(x=0)
+ax.set_xlim(1, MAX_EPISODES-1)
+ax.grid(which="both", axis='y', linestyle=':', color='lightgray', linewidth=0.5)
+ax.minorticks_on()
+ax.tick_params(which="minor", bottom=False, left=False)
+
+# Make the legend draggable
+legend1 = ax.legend()
+legend1.set_draggable(True)
+
+fig.savefig('Fig1.eps', format='eps', bbox_inches='tight')
+fig.savefig('Fig1.png', format='png', bbox_inches='tight')
+plt.show()
+
+# Plot for energy harvested
+fig2, ax = plt.subplots()
+ax.plot(eh_rewardall_ddpg, "^-", label='DDPG', linewidth=0.75 , color= 'darkblue')
+ax.plot(eh_rewardall_greedy, "o-", label='Greedy', linewidth=0.75)
+ax.plot(eh_rewardall_random, "x-", label='Random', color='black', linewidth=0.75)
+ax.set_xlabel("Episodes")
+ax.set_ylabel("Energy Harvested (J)")
+ax.legend()
+ax.margins(x=0)
+ax.set_xlim(1, MAX_EPISODES-1)
+ax.set_yscale('log')
+ax.grid(which="both", axis='y', linestyle=':', color='lightgray', linewidth=0.5)
+ax.minorticks_on()
+ax.tick_params(which="minor", bottom=False, left=False)
+
+# Make the legend draggable
+legend2 = ax.legend()
+legend2.set_draggable(True)
+
+fig2.savefig('Fig2.eps', format='eps', bbox_inches='tight')
+fig2.savefig('Fig2.png', format='png', bbox_inches='tight')
+
+plt.show()
+
+fig3, ax = plt.subplots()
+ax.plot(ee_rewardall_ddpg, "^-", label='DDPG', linewidth=0.75 , color= 'darkblue')
+ax.plot(ee_rewardall_greedy, "o-", label='Greedy', linewidth=0.75, color= 'orange')
+ax.plot(ee_rewardall_random, "x-", label='Random', color='black', linewidth=0.75)
+ax.set_xlabel("Episodes")
+ax.set_ylabel("Average Energy Efficiency (b/J)")
+ax.legend()
+ax.margins(x=0)
+ax.set_xlim(0, MAX_EPISODES)
+ax.grid(which="both", axis='y', linestyle=':', color='lightgray', linewidth=0.5)
+ax.minorticks_on()
+ax.tick_params(which="minor", bottom=False, left=False)
+
+# Add a zoomed-in inset plot for Greedy and Random comparison
+axins = inset_axes(ax, width="18%", height="13%", loc=4, borderpad=4)  # Adjust size and location
+axins.plot(ee_rewardall_greedy, "o-", label='Greedy', color='orange', linewidth=0.75)
+axins.plot(ee_rewardall_random, "x-", label='Random', color='black', linewidth=0.75)
+
+# Set limits for the zoomed-in section that matches the data in the main plot
+x1, x2 = 165, 175 # Adjust based on your data range for episodes
+y1, y2 = 40, 60  # Adjust to fit the energy efficiency range
+axins.set_xlim(x1, x2)
+axins.set_ylim(y1, y2)
+
+# Add grid and adjust ticks for the inset
+axins.grid(which="both", axis='y', linestyle=':', color='lightgray', linewidth=0.5)
+axins.set_xticks([165, 170, 175])  # Adjust to your actual data range
+axins.set_yticks([45, 50, 55])    # Adjust y-axis ticks based on zoomed data
+
+# Highlight the zoomed region on the main plot with a rectangle
+rect = Rectangle((x1, -5000), x2-x1, 10000, linewidth=1, edgecolor='black', facecolor='none', linestyle='--')
+ax.add_patch(rect)
+
+con = ConnectionPatch(xyA=((x1 + x2) / 2, (y1 + y2) / 2 + 5000 ), coordsA=ax.transData,
+                      xyB=(x1 + 4, y1), coordsB=axins.transData,
+                      arrowstyle='->', color='black', lw=1)
+
+axins.add_artist(con)
+
+
+# Move the legend to the bottom left
+legend3 = ax.legend(loc='lower left', bbox_to_anchor=(0.1, 0.1), edgecolor="black")
+legend3.get_frame().set_alpha(None)
+legend3.get_frame().set_facecolor((1, 1, 1, 0))
+legend3.set_draggable(True)
+
+# Save figures with different formats
+fig3.savefig('Fig3.eps', format='eps', bbox_inches='tight')
+fig3.savefig('Fig3.png', format='png', bbox_inches='tight')
+
+plt.show()
